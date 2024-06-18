b8d9a5c484fd421599d6332875a291360ccac1ae
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-18 15:04:21 -0700

    df to dict conversion in read_func

--- read_func.py
+++ read_func.py
@@ -3,5 +3,19 @@ import pandas as pd
 # variable that can be changed (ensure that the header match the sample)
 CSV_file = "sample_data_multiple_links.csv"
 
-
+# create df 
 df = pd.read_csv(CSV_file, index_col = 0)
+
+
+# convert the df to dictionary
+df_dict = df.to_dict()
+
+# checkpoint: YES
+# print(df_dict)
+
+
+# print(list(df_dict.keys())[0])
+
+
+maindict = df_dict[list(df_dict.keys())[0]]
+print(maindict)
