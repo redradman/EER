1e49fbd032a8061ece689ec1f8887470d671a66b
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-18 15:09:07 -0700

    parse links in csv complete

--- read_func.py
+++ read_func.py
@@ -10,12 +10,16 @@ df = pd.read_csv(CSV_file, index_col = 0)
 # convert the df to dictionary
 df_dict = df.to_dict()
 
-# checkpoint: YES
-# print(df_dict)
-
-
-# print(list(df_dict.keys())[0])
-
-
 maindict = df_dict[list(df_dict.keys())[0]]
-print(maindict)
+
+for key in maindict.keys():
+    link_string = maindict[key]
+    # check if there is an entry 
+    if isinstance(link_string, str):
+        # generate a list and seperate the links using | as a delim 
+        parsed_list = link_string.split('|')
+        # removal of any whitespaces from all links
+        no_space_list = list(map(lambda x: x.replace(" ", ""), parsed_list))
+        # print(parsed_list)
+        # CHECKPOINT: YES
+        print(no_space_list)
