ffa2fafba7d8d2e30e520f559ea1b15bdaa9041e
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-18 18:37:48 -0700

    add the progress bar for scraping

--- script.py
+++ script.py
@@ -1,21 +1,20 @@
 import funcs
+from tqdm import tqdm
 
 # reading the 
 df, dict_with_links = funcs.link_extractor()
 
-print(dict_with_links)
-# url = dict_with_links['e@UBCV'][0]
+# print(dict_with_links)
 # print(funcs.clean_extracted_text(funcs.get_plaintext_from_url(url)))
 
 # generated database 
 programs = {}
 
-for program in dict_with_links.keys():      # access the list containing the links for each of the programs
-    for link in dict_with_links[program]:   # access each of the links in the list 
+for program in tqdm(dict_with_links.keys(), desc="Programs"):  # access the list containing the links for each of the programs
+    for link in tqdm(dict_with_links[program], desc=f"Processing {program}", leave=False):  # access each of the links in the list 
         text = funcs.get_plaintext_from_url(link)
         clean_text = funcs.clean_extracted_text(text)
         funcs.add_program(programs, program, link, clean_text)
         
         
-# for key in programs:
-    # print(programs[key][0])
+print("Web scraping is finished")
