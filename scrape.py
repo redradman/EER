8425c019712f526441c3d6609bbac5441bf228d8
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-18 19:07:03 -0700

    add comments for readability

--- scrape.py
+++ scrape.py
@@ -13,9 +13,9 @@ programs = {}
 # extract the text from the links 
 for program in tqdm(dict_with_links.keys(), desc="Programs"):  # access the list containing the links for each of the programs
     for link in tqdm(dict_with_links[program], desc=f"Processing {program}", leave=False):  # access each of the links in the list 
-        text = funcs.get_plaintext_from_url(link)
-        clean_text = funcs.clean_extracted_text(text)
-        funcs.add_program(programs, program, link, clean_text)
+        text = funcs.get_plaintext_from_url(link) # extract text
+        clean_text = funcs.clean_extracted_text(text) # clean text of spaces and unnecessary details 
+        funcs.add_program(programs, program, link, clean_text) # add the result to the database dictionary
         
         
 # funcs.save_programs_csv(programs)
