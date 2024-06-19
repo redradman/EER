fc792d71e650ed8608d800860a7bf7ee6e959361
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-18 17:53:53 -0700

    clean main/script py file

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import sys
-import requests
-from bs4 import BeautifulSoup
-import re
+
+
+
 import funcs
 
 # reading the 
@@ -13,38 +13,3 @@ print(funcs.clean_extracted_text(funcs.get_plaintext_from_url(url)))
 
 
 
-
-
-
-
-# url = input("Enter the URL to visit (include: https): ")
-
-# response = requests.get("https://entrepreneurship.ubc.ca/")
-# print(response.status_code)
-# response.raise_for_status()
-# soup = BeautifulSoup(response.content, 'html.parser')
-# text = soup.get_text().strip()
-# print(re.sub(r'\s+', ' ', text).strip())
-
-
-
-# def get_plaintext_from_url(url):
-#     try:
-#         # Send a GET request
-#         response = requests.get(url)
-#         response.raise_for_status() # halt if unsuccessful
-#         # Parse HTML via BeautifulSoup
-#         soup = BeautifulSoup(response.content, 'html.parser')
-#         # Extract and return the plain text
-#         return soup.get_text()
-#     except requests.exceptions.RequestException as e:
-#         return f"An error occurred: {e}"
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 2:
-#         print("Usage: python script.py <URL>")
-#         sys.exit(1)
-
-#     url = sys.argv[1]
-#     plaintext = get_plaintext_from_url(url)
-#     print(plaintext)
