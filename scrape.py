18f881c7c12716d265a702eedc83abdef880d689
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-22 19:22:06 -0700

    functionalize main body of scrape.py to add to func.py later

--- scrape.py
+++ scrape.py
@@ -7,18 +7,20 @@ df, dict_with_links = funcs.link_extractor()
 print(dict_with_links)
 # print(funcs.clean_extracted_text(funcs.get_plaintext_from_url(url)))
 
-# generated database used to store the extracted text with the tags
-programs = {}
 
-# extract the text from the links 
-for program in tqdm(dict_with_links.keys(), desc="Programs"):  # access the list containing the links for each of the programs
-    for link in tqdm(dict_with_links[program], desc=f"Processing {program}", leave=False):  # access each of the links in the list 
-        text = funcs.get_plaintext_from_url(link) # extract text
-        clean_text = funcs.clean_extracted_text(text) # clean text of spaces and unnecessary details 
-        funcs.add_program(programs, program, link, clean_text) # add the result to the database dictionary
+def programs_maker(): 
+# generated database used to store the extracted text with the tags
+    programs = {}
+
+    # extract the text from the links 
+    for program in tqdm(dict_with_links.keys(), desc="Programs"):  # access the list containing the links for each of the programs
+        for link in tqdm(dict_with_links[program], desc=f"Processing {program}", leave=False):  # access each of the links in the list 
+            text = funcs.get_plaintext_from_url(link) # extract text
+            clean_text = funcs.clean_extracted_text(text) # clean text of spaces and unnecessary details 
+            funcs.add_program(programs, program, link, clean_text) # add the result to the database dictionary
+    return programs
         
-        
-# funcs.save_programs_csv(programs)
+funcs.save_scraped_programs_csv(programs_maker())
 
 
 
