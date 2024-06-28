7ea5486f157b64a17cd2f95a2841bb247d402123
Radman R <113865339+redradman@users.noreply.github.com>
2024-06-27 20:41:59 -0700

    update the scrape file and update links in data, progresson nlp_label

--- nlp_label.py
+++ nlp_label.py
@@ -1,6 +1,27 @@
 import spacy
 import pandas as pd
 
-# df = pd.read_excel("cocurricular_competencies.xlsx")
 
-# print(df)
+DATA = "scraping_results.csv"
+COMPETENCIES = "cocurricular_competencies.xlsx"
+
+nlp = spacy.load("en_core_web_md")
+
+df = pd.read_excel(COMPETENCIES)
+data_df = pd.read_csv(DATA)
+
+# read the words associated with the skill
+
+# for i in range(df.shape[0]):
+    
+#     # print(f"{df.at[i, 'Comptency']}: {df.at[i, 'keyword']}")
+    
+#     keywords = df.at[i, 'keyword']
+#     keywords = keywords.split(", ")
+#     print(keywords)
+#     pass
+
+keywords = df.at[0, 'keyword']
+
+
+
