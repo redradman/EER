e90c78f72b08f76c4a12ddd2a867d316fdb9bc09
Radman <rdmnr@protonmail.com>
2025-02-17 18:46:06 -0800

    polish the syllabi mapping data. update the rendered course codes and add mapping

--- compare_scores.py
+++ compare_scores.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import re
+
+def extract_course_number(course_code):
+    """
+    Extracts the numerical part of a course code for sorting.
+    Example: 'COMM 386L' -> 386, 'BAEN 550A' -> 550
+    """
+    match = re.search(r'(\d+)', course_code)
+    return int(match.group(1)) if match else 0
 
 def load_and_prepare_data(syllabi_file, program_file):
     """
@@ -10,20 +19,53 @@ def load_and_prepare_data(syllabi_file, program_file):
     syllabi_df = pd.read_csv(syllabi_file)
     program_df = pd.read_csv(program_file)
     
-    # Pivot the dataframes to get competencies as columns and programs/courses as rows
-    syllabi_pivot = syllabi_df.pivot(
-        index='course_code',
-        columns='competency',
-        values='keyword_score'
-    )
+    # Create modified syllabi dataframe with updated course codes
+    syllabi_df_modified = syllabi_df.copy()
+    code_mapping = {
+        'COMM 280 (102-103/201-202)': 'COMM 280A',
+        'COMM 280 (101 + 203)': 'COMM 280B',
+        'COMM 382/COMR 382': 'COMM 382',
+        'COMM 383, prev 386D': 'COMM 383',
+        'COMM 387/COMR 387/COEC 387': 'COMM 387',
+        'COMM/COMR 388': 'COMM 388',
+        'COMM 466/APSC486/APSC496A/E  = 2 Term Course': 'COMM 466',
+        'COMM 482 / BAMA 503 001': 'COMM 482',
+        'COMM 486G / BAEN 510': 'COMM 486G',
+        'COMM 497/  COMR 497': 'COMM 497',
+        'BAEN 506/APSC 541': 'BAEN 506',
+        'BAEN 549 (Angele)': 'BAEN 549',
+        'BAEN 550 (Angele) - ': 'BAEN550A',
+        'BAEN 550/FCOR 502 (Fraser Pogue)': 'BAEN550B',
+        'ENPH 459 & 479': 'ENPH 459'
+    }
+    syllabi_df_modified['course_code'] = syllabi_df_modified['course_code'].replace(code_mapping)
     
+    # Pivot the dataframes
     program_pivot = program_df.pivot(
         index='program',
         columns='skill',
         values='keyword_score'
     )
     
-    return syllabi_pivot, program_pivot
+    syllabi_pivot = syllabi_df.pivot(
+        index='course_code',
+        columns='competency',
+        values='keyword_score'
+    )
+    
+    syllabi_pivot_modified = syllabi_df_modified.pivot(
+        index='course_code',
+        columns='competency',
+        values='keyword_score'
+    )
+    
+    # Sort syllabi pivot by course number (higher numbers on top)
+    syllabi_pivot_modified = syllabi_pivot_modified.sort_index(
+        key=lambda x: x.map(extract_course_number),
+        ascending=False
+    )
+    
+    return syllabi_pivot_modified, program_pivot
 
 def create_heatmap(data, title, output_file):
     """
